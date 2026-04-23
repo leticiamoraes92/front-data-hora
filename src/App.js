b7a99bd8c1d250e86c11f460fe8636e83e95c8bd
Leticia Moraes <lemoraes892@gmail.com>
2026-04-23 12:30:18 -0300

    Atualizando App com API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dataHora, setDataHora] = useState("");
+
+  useEffect(() => {
+    fetch("https://api-programa-1jkn.onrender.com")
+      .then((res) => res.json())
+      .then((dados) => {
+        setDataHora(dados.date);
+      })
+      .catch(() => {
+        setDataHora("Erro ao conectar API");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Data e Hora</h1>
+      <h2>{dataHora}</h2>
     </div>
   );
 }
